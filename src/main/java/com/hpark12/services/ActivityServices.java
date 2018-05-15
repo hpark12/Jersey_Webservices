701fb45b7649bf528e1f1853e81c97a60646d6dd
hpark12 <hyunjae.v4@gmail.com>
2018-05-15 14:28:28 -0400

    Finishing Dao

--- src/main/java/com/hpark12/services/ActivityServices.java
+++ src/main/java/com/hpark12/services/ActivityServices.java
@@ -1,24 +1,26 @@
 package com.hpark12.services;
 
 import com.hpark12.dao.ActivityDaoImplementation;
+import com.hpark12.repository.Activity;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("activities")
 public class ActivityServices {
 
-    @Path("{activityid}")
-    public void getActivity(int activityId){
-        new ActivityDaoImplementation().findActivity(activityId);
+    ActivityDaoImplementation example = new ActivityDaoImplementation();
+
+    
+    @GET                                    // http://localhost:8080/Jersey_Webservices/webapi/activities
+    @Produces(MediaType.APPLICATION_XML)
+    public List<Activity> getActivity(){
+        return example.GetAllActivities();
     }
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
 
-        return "Got it with the pipeline!";
-    }
+
 }
